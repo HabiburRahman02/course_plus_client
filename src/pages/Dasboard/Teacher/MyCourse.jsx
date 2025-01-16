12e7a8bbd7a3151fdf530cd8cf361333374eb849
Habibur Rahman <habibur88454@gmail.com>
2025-01-16 16:41:11 +0600

    delete course card and using tanstack for data load

--- src/pages/Dasboard/Teacher/MyCourse.jsx
+++ src/pages/Dasboard/Teacher/MyCourse.jsx
@@ -1,30 +1,37 @@
-import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useAuth from "../../../hooks/useAuth";
 import Heading from "../../../components/Heading";
 import MyCourseCard from "./MyCourseCard";
+import { useQuery } from "@tanstack/react-query";
 
 const MyCourse = () => {
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
-    const [myCourse, setMyCourse] = useState([])
 
-    useEffect(() => {
-        axiosSecure.get(`/course/${user?.email}`)
-            .then(res => {
-                console.log(res.data);
-                setMyCourse(res.data);
-            })
-    }, [axiosSecure, user?.email])
+    // useEffect(() => {
+    //     axiosSecure.get(`/course/${user?.email}`)
+    //         .then(res => {
+    //             console.log(res.data);
+    //             setMyCourse(res.data);
+    //         })
+    // }, [axiosSecure, user?.email])
+    const { data: myCourse, refetch } = useQuery({
+        queryKey: ['course', user?.email],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/course/${user?.email}`)
+            return res.data;
+        }
+    })
 
     return (
         <div>
-            <Heading title='My all courses here'></Heading>
+            <Heading title={`My all courses here: ${myCourse.length}`}></Heading>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-4">
                 {
                     myCourse?.map(course => <MyCourseCard
                         key={course._id}
                         course={course}
+                        refetch={refetch}
                     ></MyCourseCard>)
                 }
             </div>
--- src/pages/Dasboard/Teacher/MyCourseCard.jsx
+++ src/pages/Dasboard/Teacher/MyCourseCard.jsx
@@ -1,7 +1,35 @@
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { toast } from "react-toastify";
 
-const MyCourseCard = ({ course }) => {
+const MyCourseCard = ({ course, refetch }) => {
+    const axiosSecure = useAxiosSecure();
     const { name, image, description, email, price, status, title, _id } = course || {}
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/courseDelete/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            toast.success('Deleted course successfully')
+                        }
+                    })
+
+            }
+        });
+    }
+
     return (
         <div className="card w-full card-compact bg-base-100 shadow-xl">
             <figure>
@@ -16,12 +44,12 @@ const MyCourseCard = ({ course }) => {
                 <p><span className="font-semibold">Email:</span> {email}</p>
                 <p><span className="font-semibold">Description:</span> {description}</p>
                 <p><span className="font-semibold">Price:</span> ${price}</p>
-                <p><span className="font-semibold">Status:</span> {status}</p>
+                <p><span className="font-semibold">Status:</span> <span className="bg-pink-200 rounded-full px-2 py-1">{status}</span></p>
                 <div className="card-actions justify-between">
                     <Link to={`/dashboard/updateCourse/${_id}`}>
                         <button className="btn btn-primary">Update</button>
                     </Link>
-                    <button className="btn btn-secondary">Delete</button>
+                    <button onClick={() => handleDelete(_id)} className="btn btn-secondary">Delete</button>
                 </div>
                 {
                     status === 'approved' ? <button className="btn btn-outline mt-3">See Progress</button> : <button disabled className="btn btn-outline mt-3">See Progress</button>
