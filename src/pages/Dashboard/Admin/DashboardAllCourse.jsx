c9392f429fac8c856ca3d404392b5384f1b3a836
Habibur Rahman <habibur88454@gmail.com>
2025-01-17 23:52:33 +0600

    rejected course

--- src/pages/Dashboard/Admin/DashboardAllCourse.jsx
+++ src/pages/Dashboard/Admin/DashboardAllCourse.jsx
@@ -1,10 +1,30 @@
+import { toast } from "react-toastify";
 import Heading from "../../../components/Heading";
 import useAllCourse from "../../../hooks/useAllCourse";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 
 const DashboardAllCourse = () => {
     const [courses, loading, refetch] = useAllCourse();
-    console.log(courses);
+    const axiosSecure = useAxiosSecure();
+
+    const handleApproved = async (id) => {
+        const res = await axiosSecure.patch(`/allCoursesApproved/${id}`)
+        if (res.data.modifiedCount > 0) {
+            toast.success('Approved this course!!')
+            refetch();
+        }
+    }
+
+    const handleRejected = async (id) => {
+        console.log(id);
+        const res = await axiosSecure.patch(`/allCoursesRejected/${id}`)
+        if (res.data.modifiedCount > 0) {
+            toast.success('Rejected this course!!')
+            refetch();
+        }
+    }
+
     return (
         <div>
             <Heading title='All courses here'></Heading>
@@ -25,28 +45,34 @@ const DashboardAllCourse = () => {
                     </thead>
                     <tbody>
                         {
-                            courses?.map((user, i) => <tr key={user._id}>
+                            courses?.map((course, i) => <tr key={course._id}>
                                 <th>{i + 1}</th>
                                 <td>
                                     <div className="flex items-center gap-3">
                                         <div className="avatar">
                                             <div className="mask mask-squircle h-12 w-12">
                                                 <img
-                                                    src={user.image}
+                                                    src={course.image}
                                                     alt="Avatar Tailwind CSS Component" />
                                             </div>
                                         </div>
                                     </div>
                                 </td>
-                                <td>{user.title}</td>
-                                <td>{user.email}</td>
-                                <td>{user.description.slice(0, 10)}</td>
-                                <td>{user.status}</td>
+                                <td>{course.title}</td>
+                                <td>{course.email}</td>
+                                <td>{course.description.slice(0, 10)}</td>
+                                <td>{course.status}</td>
                                 <td>
-                                    <button className="btn btn-secondary">Approve</button>
+                                    <button
+                                        onClick={() => handleApproved(course._id)}
+                                        disabled={course.status === 'approved'}
+                                        className="btn btn-secondary" >Approve</button>
                                 </td>
                                 <td>
-                                    <button className="btn btn-accent">Reject</button>
+                                    <button
+                                        onClick={() => handleRejected(course._id)}
+                                        disabled={course.status === 'rejected'}
+                                        className="btn btn-accent">Reject</button>
                                 </td>
                                 <td>
                                     <button className="btn btn-primary">Progress</button>
