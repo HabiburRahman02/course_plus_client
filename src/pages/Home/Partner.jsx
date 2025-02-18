c4fe19e761cdb11899d8feb47d1bef57698ad214
Habibur Rahman <habibur88454@gmail.com>
2025-02-18 19:09:05 +0600

    our partner page added

--- src/pages/Home/Partner.jsx
+++ src/pages/Home/Partner.jsx
@@ -3,7 +3,7 @@ const Partner = ({ img }) => {
     return (
 
         <div >
-            < img className="rounded-full h-56 w-56 object-cover border-2 border-[rgb(37,168,214)] p-1" src={img} alt="" />
+            < img className="rounded-full h-48 w-48 object-cover border-2 border-[rgb(37,168,214)] p-1" src={img} alt="" />
             {/* <h3 className="text-2xl font-semibold mt-2">{userName}</h3> */}
         </div >
     );
--- src/pages/Home/Partners.jsx
+++ src/pages/Home/Partners.jsx
@@ -1,10 +1,11 @@
-
-
 import person1 from '../../assets/partners/partner1.png'
 import person2 from '../../assets/partners/partner2.png'
 import person3 from '../../assets/partners/partner3.png'
-import person4 from '../../assets/partners/partner4.jpeg'
+import person4 from '../../assets/partners/partner4.png'
 import person5 from '../../assets/partners/partner5.png'
+import person6 from '../../assets/partners/partner6.png'
+import person7 from '../../assets/partners/partner7.webp'
+import person8 from '../../assets/partners/partner8.jpeg'
 
 import Marquee from 'react-fast-marquee';
 import Partner from './Partner'
@@ -23,9 +24,9 @@ const Partners = () => {
                             <Partner img={person3} ></Partner>
                             <Partner img={person4} ></Partner>
                             <Partner img={person5} ></Partner>
-                            <Partner img={person1} ></Partner>
-                            <Partner img={person2} ></Partner>
-                            <Partner img={person3} ></Partner>
+                            <Partner img={person6} ></Partner>
+                            <Partner img={person7} ></Partner>
+                            <Partner img={person8} ></Partner>
                         </div>
                     </Marquee>
                 </div>
