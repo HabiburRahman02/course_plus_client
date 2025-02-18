25895b77469f0b02fb61dd6c195d7053cd84a0c6
Habibur Rahman <habibur88454@gmail.com>
2025-02-18 18:48:28 +0600

    banner update

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,8 +1,8 @@
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 import banner1 from '../../assets/banner/banner1.jpg'
-import banner2 from '../../assets/banner/banner2.png'
-import banner3 from '../../assets/banner/banner2.png'
+import banner2 from '../../assets/banner/banner2.jpg'
+import banner3 from '../../assets/banner/banner3.jpg'
 import { Link } from 'react-router-dom';
 
 import withAutoplay from 'react-awesome-slider/dist/autoplay';
@@ -15,7 +15,7 @@ const Banner = () => {
     return (
         <div className=''>
             <AutoplaySlider
-                // play={true}
+                play={true}
                 cancelOnInteraction={false}
                 interval={2000}
                 className='w-full md:h-[550px] bg-fixed h-[520px]' >
@@ -32,25 +32,25 @@ const Banner = () => {
                             <div className="md:w-2/3">
                                 <Fade direction='down' duration={2000}>
                                     <p className="mb-5 md:text-xl text-lg font-bold uppercase">
-                                        create a online Group study site
+                                        Build Your Own Interactive Learning Platform
                                     </p>
                                 </Fade>
                                 <Fade direction='up' duration={2000}>
-                                    <h1 className="mb-12 text-5xl md:text-6xl font-bold uppercase">so easy when <span className='text-[#1ba590]'>udetor</span> appears.</h1>
-
+                                    <h1 className="mb-12 text-4xl md:text-5xl font-bold uppercase">
+                                        Transform Learning with <span className='text-[#1ba590]'>Course Plus</span>.
+                                    </h1>
                                 </Fade>
-                                <Link to='/assignments'>
+                                <Link to='/allCourses'>
                                     <Fade direction='up' duration={2000}>
-                                        <button className="uppercase bg-cyan-700 py-2 px-12  text-white font-bold 
-                                    border-[2px]
-                                    border-transparent hover:border-[2px] hover:border-white hover:bg-transparent transition-all rounded-full">
-                                            Get Started
+                                        <button className="uppercase bg-cyan-700 py-2 px-12 text-white font-bold 
+                        border-[2px] border-transparent hover:border-[2px] hover:border-white hover:bg-transparent 
+                        transition-all rounded-full">
+                                            Explore Now
                                         </button>
                                     </Fade>
                                 </Link>
                             </div>
                             <div className='md:w-1/3'>
-
                             </div>
                         </div>
                     </div>
@@ -62,31 +62,31 @@ const Banner = () => {
                         backgroundRepeat: 'no-repeat',
                         backgroundSize: 'cover'
                     }}>
-                    <div className="hero-overlay bg-opacity-80"></div>
+                    <div className="hero-overlay bg-opacity-70"></div>
                     <div className="hero-content text-neutral-content">
                         <div className='flex justify-start items-center'>
                             <div className="md:w-2/3">
                                 <Fade direction='down' duration={2000}>
                                     <p className="mb-5 md:text-xl text-lg font-bold uppercase">
-                                        create a online Group study site
+                                        Empower Your Learning with Advanced Resources
                                     </p>
                                 </Fade>
                                 <Fade direction='up' duration={2000}>
-                                    <h1 className="mb-12 text-5xl md:text-6xl font-bold uppercase">so easy when <span className='text-[#1ba590]'>udetor</span> appears.</h1>
-
+                                    <h1 className="mb-12 text-4xl md:text-5xl font-bold uppercase">
+                                        Elevate Your Skills with <span className='text-[#1ba590]'>Course Master</span>.
+                                    </h1>
                                 </Fade>
-                                <Link to='/assignments'>
+                                <Link to='/exploreCourses'>
                                     <Fade direction='up' duration={2000}>
-                                        <button className="uppercase bg-cyan-700 py-2 px-12  text-white font-bold 
-                                    border-[2px]
-                                    border-transparent hover:border-[2px] hover:border-white hover:bg-transparent transition-all rounded-full">
-                                            Get Started
+                                        <button className="uppercase bg-cyan-700 py-2 px-12 text-white font-bold 
+                        border-[2px] border-transparent hover:border-[2px] hover:border-white hover:bg-transparent 
+                        transition-all rounded-full">
+                                            Explore Now
                                         </button>
                                     </Fade>
                                 </Link>
                             </div>
                             <div className='md:w-1/3'>
-
                             </div>
                         </div>
                     </div>
@@ -98,35 +98,38 @@ const Banner = () => {
                         backgroundRepeat: 'no-repeat',
                         backgroundSize: 'cover'
                     }}>
-                    <div className="hero-overlay bg-opacity-0"></div>
+                    <div className="hero-overlay bg-opacity-70"></div>
                     <div className="hero-content text-neutral-content">
                         <div className='flex justify-start items-center'>
                             <div className="md:w-2/3">
                                 <Fade direction='down' duration={2000}>
                                     <p className="mb-5 md:text-xl text-lg font-bold uppercase">
-                                        create a online Group study site
+                                        Empower Your Learning with Advanced Resources
                                     </p>
                                 </Fade>
                                 <Fade direction='up' duration={2000}>
-                                    <h1 className="mb-12 text-5xl md:text-6xl font-bold uppercase">so easy when <span className='text-[#1ba590]'>udetor</span> appears.</h1>
-
+                                    <h1 className="mb-12 text-4xl md:text-5xl font-bold uppercase">
+                                        Elevate Your Skills with <span className='text-[#1ba590]'>Course Master</span>.
+                                    </h1>
                                 </Fade>
-                                <Link to='/assignments'>
+                                <Link to='/allCourses'>
                                     <Fade direction='up' duration={2000}>
-                                        <button className="uppercase bg-cyan-700 py-2 px-12  text-white font-bold 
-                                    border-[2px]
-                                    border-transparent hover:border-[2px] hover:border-white hover:bg-transparent transition-all rounded-full">
-                                            Get Started
+                                        <button className="uppercase bg-cyan-700 py-2 px-12 text-white font-bold 
+                        border-[2px] border-transparent hover:border-[2px] hover:border-white hover:bg-transparent 
+                        transition-all rounded-full">
+                                            Explore Now
                                         </button>
                                     </Fade>
                                 </Link>
                             </div>
                             <div className='md:w-1/3'>
-
                             </div>
                         </div>
                     </div>
                 </div>
+
+
+
             </AutoplaySlider>
 
         </div>
